54c8878f0d195f0e4841eab0776ba58f691a9663
DavitPH <davitph0@gmail.com>
2022-10-27 16:09:22 +0700

    Add project files.

---
+++ Linaear_dan_Binary_Search/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Linaear_dan_Binary_Search
+{
+    class Program
+    {
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
