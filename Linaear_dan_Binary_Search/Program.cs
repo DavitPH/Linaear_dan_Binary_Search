87b36c93aed24031dd83af7d0ed41d4ae23a309a
DavitPH <davitph0@gmail.com>
2022-10-27 16:42:58 +0700

    Membuat method publik BinarySearch berisi algoritma Binarry Search

--- Linaear_dan_Binary_Search/Program.cs
+++ Linaear_dan_Binary_Search/Program.cs
@@ -32,20 +32,68 @@ namespace Linaear_dan_Binary_Search
             Console.WriteLine("Enter Array Element ");
             Console.WriteLine("--------------------");
 
-            for(i = 0; i < n; i++)
+            for (i = 0; i < n; i++)
             {
-                Console.WriteLine("<"+ (i+1) + ">");
+                Console.WriteLine("<" + (i + 1) + ">");
                 string s1 = Console.ReadLine();
                 arr[i] = Int32.Parse(s1);
             }
-
-
         }
 
+
+        public void BinarySearch()
+        {
+            char ch;
+
+            do
+            {
+                //Accept the number to be searched
+                Console.Write("\n Enter the Element you want to search : ");
+                int item = Convert.ToInt32(Console.ReadLine());
+
+                //Apply binary search
+                int lowerbound = 0;
+                int upperbound = 0;
+
+                //obtained the index of the middle element in the array
+                int mid = (lowerbound + upperbound) / 2;
+                int ctr = 1;
+
+                //loop the search for the element in the array
+                while ((item  != arr[mid]) && (ctr <= upperbound))
+                {
+                    if(item == arr[mid])
+                    {
+                        lowerbound = mid + 1;
+                    else
+                         upperbound = mid - 1;
+
+
+                        mid = (lowerbound + upperbound) / 2;
+                        ctr++;
+                    }
+
+                    if (item == arr[mid])
+                    {
+                        Console.WriteLine("\n" + item.ToString() + "found at position" + (mid + 1).ToString());
+                    else
+                            Console.WriteLine("\n" + item.ToString() + "not found in the array\n");
+                        Console.WriteLine("\nNumber of Comparasion : " + ctr);
+
+                        Console.WriteLine("\nContinue search (y/n) : ");
+                        ch = char.Parse(Console.ReadLine());
+                    } while ((ch == 'y')) ;
+                }
+
+            }
+
+        }
+    
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
     }
+
 }
